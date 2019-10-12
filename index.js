c7b5b8938f1c66af5a6dd99a288693d5e77a4881
skitterm <skitterman@esri.com>
2019-10-11 20:25:32 -0700

    Clean up now-ignored js files

--- index.js
+++
@@ -1,6 +0,0 @@
-const Session = require("./patterns/creational/singleton");
-
-const sessionOne = Session.Instance("john");
-const sessionTwo = Session.Instance("jane");
-console.log(sessionOne.getName());
-console.log(sessionTwo.getName());
